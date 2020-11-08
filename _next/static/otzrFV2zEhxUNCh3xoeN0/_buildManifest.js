a9db8726a0ab7eef68c3be2d9067b4b40b8226d3
wasinpp <pp.wasin@gmail.com>
2020-11-08 16:58:32 +0000

    Deploying to gh-pages from  @ 5bf5917fba38b2e34856996bbb0aa2e5a7f3027d 🚀

--- _next/static/otzrFV2zEhxUNCh3xoeN0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-67a97c065d0de794c474.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c032e99ed416dc555a7d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
