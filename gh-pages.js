e3584d75e1897f11009a6c938fbf08d31b9187d0
wasin.p <wasin.p@getthailand.com>
2020-11-08 23:49:10 +0700

    Intitial project: Next.js tailwind.css gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,13 @@
+// let ghpages = require("gh-pages");
+// let moment = require("moment");
+// ghpages.publish(
+//   "out",
+//   {
+//     branch: "main",
+//     dotfiles: true,
+//     message: `🚀 Deploy [${moment().format("DD/MM/YY")}]`,
+//   },
+//   () => {
+//     console.log("yayyy");
+//   }
+// );
